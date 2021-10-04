fd2bd1d5e2939b4cdf60a4d8d8314366784b18b9
Efraim Cancio <efraimfonseca@gmail.com>
2021-10-03 23:47:50 -0300

    responsiveness

--- main.js
+++ main.js
@@ -21,13 +21,13 @@ for(const link of links) {
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
+function changeHeaderWhenScroll() {
     if(window.scrollY >= navHeight) {
         header.classList.add('scroll')
     } else {
         header.classList.remove('scroll')
     }
-})
+}
 
 /* Testimonials sliders (swiper)*/
 const swiper = new Swiper('.swiper', {
@@ -37,11 +37,17 @@ const swiper = new Swiper('.swiper', {
     },
     mousewheel: true,
     keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
 })
 
 /* Scroll reveal*/
 const scrollReveal = ScrollReveal({
-    orientation: 'top',
+    origin: 'top',
     distance: '30px',
     duration: 1000,
     reset: true
@@ -59,10 +65,41 @@ scrollReveal.reveal(
 
 /* back to the top */
 const backToTopButton = document.querySelector('.back-to-top')
-window.addEventListener('scroll', function () {
+function backToTop() {
     if (window.scrollY >= 560) {
         backToTopButton.classList.add('show')
     } else {
         backToTopButton.classList.remove('show')
     }
+}
+
+/* Lintk active */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+    
+    for( const section of sections ) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if (checkpointStart && checkpointEnd) {
+            document
+             .querySelector('nav ul li a[href*=' + sectionId + ']')
+             .classList.add('active')
+        } else {
+            document
+             .querySelector('nav ul li a[href*=' + sectionId + ']')
+             .classList.remove('active')
+        }
+    }
+}
+
+/* When scroll */
+window.addEventListener ('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()
 })
